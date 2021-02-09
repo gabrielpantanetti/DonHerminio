d7a62374a05780ecc8790952534154e7f99407f4
Pantanetti <DPantanetti@traditum.com>
2021-02-09 18:12:28 -0300

    Primer commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+declare var $: any;
+declare function init($);
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+export class AppComponent implements OnInit {
+  title = 'DonHerminio';
+
+
+
+  ngOnInit(): void {
+
+    // SLIDER
+    init($);
+
+  }
+}
